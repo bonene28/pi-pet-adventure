124d6f2ea5745587f1dfc4ec98ce61bcb52d02d9
bonene28 <yvonnelagonoy25@gmail.com>
2026-08-03 02:27:53 +0800

    Create game.js

---
+++ game.js
@@ -0,0 +1,7 @@
+// Pi Pet Adventure Version 1.0
+
+const startBtn = document.getElementById("startBtn");
+
+startBtn.addEventListener("click", function () {
+    alert("Welcome to Pi Pet Adventure!\n\nPet Selection - Coming Soon!");
+});
