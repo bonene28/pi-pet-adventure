bf589b481decaf0f0fd97e9910742709f8f4aaf9
bonene28 <yvonnelagonoy25@gmail.com>
2026-08-03 02:32:20 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,17 @@
 // Pi Pet Adventure Version 1.0
 
 const startBtn = document.getElementById("startBtn");
+const settingBtn = document.getElementById("settingBtn");
+const exitBtn = document.getElementById("exitBtn");
 
-startBtn.addEventListener("click", function () {
-    alert("Welcome to Pi Pet Adventure!\n\nPet Selection - Coming Soon!");
-});
+startBtn.onclick = function () {
+    alert("🐾 Welcome!\n\nNext: Pet Selection");
+};
+
+settingBtn.onclick = function () {
+    alert("⚙ Settings\n\nComing Soon!");
+};
+
+exitBtn.onclick = function () {
+    alert("👋 Thanks for playing Pi Pet Adventure!");
+};
